361853535b135834958dd6f587e67084b63251a3
ahmadnizarriyansyah <104213213+ahmadnizarriyansyah@users.noreply.github.com>
2025-06-04 16:56:19 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# tugas-bdd
-Tugas 4
+# BDD Sauce Demo
+
+This project is a simple demo of how to use Cucumber and Cypress together to automate a simple web application.
+
+target: https://www.saucedemo.com/
+
+## Getting Started
+
+### Prerequisites
+
+- Node.js
+- NPM
+
+### Installing
+
+1. Clone the repo
+2. Run `npm install` to install dependencies
+3. Run `npm test` to run the tests
---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
+
+module.exports = defineConfig({
+  e2e: {
+    specPattern: "cypress/e2e/**/*.feature",
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on("file:preprocessor", createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      }));
+
+      return config;
+    },
+  },
+});
